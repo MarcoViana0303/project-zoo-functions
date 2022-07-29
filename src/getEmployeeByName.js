2c078c6df7fea84d849f2ba9bac7db8d7df79cef
Marco Viana <marcoviana100@gmail.com>
2022-07-29 16:05:01 -0300

    Requizito 3 finalizado com lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const colaboradores = data.employees;
+  const nome = colaboradores.find((elemento) => elemento.firstName
+    === employeeName || elemento.lastName === employeeName);
+
+  return nome;
 }
 
 module.exports = getEmployeeByName;
