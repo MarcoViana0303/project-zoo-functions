a5e6f28e9de6d95e3fdcbc88a245ac4e7570af26
Marco Viana <marcoviana100@gmail.com>
2022-07-29 15:14:09 -0300

    Requizito 2 finalizado com lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const especies = data.species;
+  const animais = especies.find((elemento) => elemento.name === animal);
+  const animaisIdade = animais.residents;
+  const verdadeiroFalso = animaisIdade.every((element) => element.age > age);
+
+  return verdadeiroFalso;
 }
 
 module.exports = getAnimalsOlderThan;
