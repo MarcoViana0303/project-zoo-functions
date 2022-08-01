ff9a1f852a52da9fa9ebaa1f19f4f273eb427b5c
Marco Viana <marcoviana100@gmail.com>
2022-08-01 18:43:19 -0300

    Requizito 8 finalizado com lint 2.0

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,46 @@
 const data = require('../data/zoo_data');
 
+const entrants2 = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
   // seu código aqui
+  const child1 = entrants.filter((elemento) => elemento.age < 18);
+  const adult1 = entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50);
+  const senior1 = entrants.filter((elemento) => elemento.age >= 50);
+
+  const teste = entrants.reduce((acc, elemento) => {
+    acc.child = child1.length;
+    acc.adult = adult1.length;
+    acc.senior = senior1.length;
+    return acc;
+  }, {});
+
+  return teste;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
 
+  if (!entrants || Array.isArray(entrants) === false) {
+    return 0;
+  }
+
+  const child = entrants.filter((elemento) => elemento.age < 18);
+  const adult = entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50);
+  const senior = entrants.filter((elemento) => elemento.age >= 50);
+  let preco = 0;
+  preco = (child.length * data.prices.child)
+    + (adult.length * data.prices.adult) + (senior.length * data.prices.senior);
+  return preco;
 }
+
+console.log(calculateEntry(entrants2));
 
 module.exports = { calculateEntry, countEntrants };
