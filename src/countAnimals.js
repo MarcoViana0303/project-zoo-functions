c106fe41f5979d7e90a33626a5c1caaf36b44a16
Marco Viana <marcoviana100@gmail.com>
2022-08-04 20:18:13 -0300

    Requizito 10 finalizado com lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,16 +2,21 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-  const especies = data.species;
+
   if (!animal) {
-    const animaisObjeto = especies.reduce((acc, elemento) => {
+    const animaisObjeto = data.species.reduce((acc, elemento) => {
       acc[elemento.name] = elemento.residents.length;
 
       return acc;
     }, {});
     return animaisObjeto;
   }
+  if (Object.keys(animal[1]) === 'sex') {
+    // const femea = data.species.filter((elemento) => elemento.residents.residents);
+  }
+
+  const animais = data.species.find((elemento) => elemento.name === animal.specie).residents.length;
+  return animais;
 }
 
-console.log(countAnimals());
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,8 +2,19 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const pessoaId = data.employees.find((elemento) => elemento.id.includes(id));
-  const primeiroAnimal = pessoaId.responsibleFor;
-  return primeiroAnimal;
+  const primeiroAnimal = pessoaId.responsibleFor.find((elemento) => elemento);
+  const animalRaca = data.species.filter((elemento) => elemento.id === primeiroAnimal);
+  const animal = animalRaca.map((elemento) => elemento.residents);
+  const exemplo = animal.find((elemento) => elemento);
+  const outroExemplo = exemplo.reduce((acc, curr) => {
+    if (acc.age < curr.age) {
+      return curr;
+    }
+
+    return acc;
+  });
+
+  return Object.values(outroExemplo);
 }
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
