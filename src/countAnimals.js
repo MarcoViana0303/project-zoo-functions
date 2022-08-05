e98fcf2622bc61bcd3281e5a3d3be51bf61456ee
Marco Viana <marcoviana100@gmail.com>
2022-08-04 21:37:45 -0300

    Requizito 5 finalizado com lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,19 +4,20 @@ function countAnimals(animal) {
   // seu código aqui
 
   if (!animal) {
-    const animaisObjeto = data.species.reduce((acc, elemento) => {
+    return data.species.reduce((acc, elemento) => {
       acc[elemento.name] = elemento.residents.length;
 
       return acc;
     }, {});
-    return animaisObjeto;
   }
-  if (Object.keys(animal[1]) === 'sex') {
-    // const femea = data.species.filter((elemento) => elemento.residents.residents);
+  const animais = data.species.find((elemento) => elemento.name === animal.specie).residents;
+  if (Object.keys(animal).length === 1) {
+    return animais.length;
   }
 
-  const animais = data.species.find((elemento) => elemento.name === animal.specie).residents.length;
-  return animais;
+  return animais.filter((elemento) => elemento.sex === animal.sex).length;
 }
+
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
